ec6b9ec10dbb192c1b9a9dcabce8ffc4b8883263
Filip Bergman <fi5731be-s@student.lu.se>
2021-05-07 18:55:25 +0200

    optAlignments works

--- assignment2/main.hs
+++ assignment2/main.hs
@@ -23,8 +23,8 @@ mcsLength xs ys = mcsLen (length xs) (length ys)
 
 -- A
 similarityScore :: String -> String -> Int
-similarityScore _ [] = scoreMismatch
-similarityScore [] _ = scoreMismatch
+similarityScore xs [] = scoreSpace * length xs
+similarityScore [] ys = scoreSpace * length ys
 similarityScore (x:xs) (y:ys) = maximum [similarityScore xs ys + score x y, similarityScore xs (y:ys) + score x '-', similarityScore (x:xs) ys + score '-' y]
 
 score :: Char -> Char -> Int
@@ -42,15 +42,56 @@ attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
 
 -- C
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
-maximaBy valueFcn xs = [x | x <- xs, valueFcn x >= maximum (map valueFcn xs)]
+maximaBy valueFcn xs = [x | x <- xs, valueFcn x == maximum (map valueFcn xs)]
 
 type AlignmentType = (String,String)
 
 -- D
 -- Uncurry makes similarityScore take a tuple as argument instead of 2 strings
 optAlignments :: String -> String -> [AlignmentType]
-optAlignments [] [] = []
-optAlignments x [] = optAlignments x ['-']
-optAlignments [] y = optAlignments ['-'] y
-optAlignments (x:xs) (y:ys) = maximaBy (uncurry similarityScore) (concat [attachHeads x y (optAlignments xs ys), attachHeads x '-' (optAlignments xs (y:ys)), attachHeads '-' y (optAlignments (x:xs) ys)])
---optAlignments (x:xs) (y:ys) = maximaBy (similarityScore ) []
+optAlignments [] [] = [([], [])]
+optAlignments (x:xs) [] = attachHeads x '-' (optAlignments xs [])
+optAlignments [] (y:ys) = attachHeads '-' y (optAlignments [] ys)
+optAlignments (x:xs) (y:ys) = maximaBy (uncurry simScore) (concat [attachHeads x y (optAlignments xs ys), attachHeads x '-' (optAlignments xs (y:ys)), attachHeads '-' y (optAlignments (x:xs) ys)])
+
+simScore :: String -> String -> Int
+simScore xs [] = scoreMismatch * length xs
+simScore [] ys = scoreMismatch * length ys
+simScore (x:xs) (y:ys) = simScore xs ys + score x y
+
+optoptAlignments :: String -> String -> [AlignmentType]
+optoptAlignments [] [] = [([], [])]
+optoptAlignments (x:xs) [] = attachHeads x '-' (optoptAlignments xs [])
+optoptAlignments [] (y:ys) = attachHeads '-' y (optoptAlignments [] ys)
+optoptAlignments (x:xs) (y:ys) = maximaBy (uncurry simScore) (concat [attachHeads x y (optoptAlignments xs ys), attachHeads x '-' (optoptAlignments xs (y:ys)), attachHeads '-' y (optoptAlignments (x:xs) ys)])
+
+
+optoptAlignments :: Eq a => [a] -> [a] -> [AlignmentType]
+optoptAlignments xs ys = tableVal (length xs) (length ys)
+  where
+    tableVal i j = mcsTable!!i!!j
+    mcsTable = [[ tableEntry i j | j<-[0..]] | i<-[0..] ]
+       
+    tableEntry :: Int -> Int -> (Int, [AlignmentType])
+    tableEntry a 0 = (scoreSpace * a, )
+    tableEntry 0 b = (scoreSpace * b, )
+    tableEntry i j
+      | x == y    = 1 + tableVal (i-1) (j-1)
+      | otherwise = max (tableVal i (j-1)) 
+                        (tableVal (i-1) j)
+      where
+         x = xs!!(i-1)
+         y = ys!!(j-1)
+
+
+outputOptAlignments :: String -> String -> IO ()
+outputOptAlignments string1 string2 = do 
+  let ali = (optAlignments string1 string2)
+  putStrLn ("There are " ++ show (length ali) ++ " optimal alignments:") 
+  outputHelper ali
+
+outputHelper :: [AlignmentType] -> IO ()
+outputHelper [] = putStrLn "\n"
+outputHelper (x:xs) = do
+  putStrLn ((fst x) ++ "\n" ++ (snd x) ++ "\n")
+  outputHelper xs
